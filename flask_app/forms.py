e67d8b2b2b2bdbd774c70487e640ada5ae13be58
ThatGamingFella <78178170+ThatGamingFella@users.noreply.github.com>
2021-05-11 03:20:47 -0400

    Add files via upload

---
+++ flask_app/forms.py
@@ -0,0 +1,102 @@
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
+from wtforms import StringField, IntegerField, SubmitField, TextAreaField, PasswordField, SelectField
+from wtforms.validators import (InputRequired, DataRequired, NumberRange, Length, Email,
+                                EqualTo, ValidationError, Optional)
+import pyotp
+
+from .models import User
+
+
+class SearchTeamForm(FlaskForm):
+    search_query = SelectField('Team', choices=[('ARI', 'Arizona Cardinals'), ('ATL', 'Atlanta Falcons'),
+                                                ('BAL', 'Baltimore Ravens'), ('BUF', 'Buffalo Bills'),
+                                                ('CAR', 'Carolina Panthers'), ('CHI', 'Chicago Bears'),
+                                                ('CIN', 'Cincinnati Bengals'), ('CLE', 'Cleveland Browns'),
+                                                ('DAL', 'Dallas Cowboys'), ('DEN', 'Denver Broncos'),
+                                                ('DET', 'Detroit Lions'), ('GB', 'Green Bay Packers'),
+                                                ('HOU', 'Houston Texans'), ('IND', 'Indianapolis Colts'),
+                                                ('JAC', 'Jacksonville Jaguars'), ('KC', 'Kansas City Chiefs'),
+                                                ('LA', 'Los Angelos Rams'), ('LAC', 'Los Angelos Chargers'),
+                                                ('MIA', 'Miami Dolphins'), ('MIN', 'Minnesota Vikings'),
+                                                ('NE', 'New England Patriots'), ('NO', 'New Orleans Saints'),
+                                                ('NYG', 'New York Giants'), ('NYJ', 'New York Jets'),
+                                                ('OAK', 'Oakland Raiders'), ('PHI', 'Philadelphia Eagles'),
+                                                ('PIT', 'Pittsburgh Steelers'), ('SEA', 'Seattle Seahawks'),
+                                                ('SF', 'San Francisco 49ers'), ('TB', 'Tampa Bay Buccaneers'),
+                                                ('TEN', 'Tennessee Titans'), ('WAS', 'Washington Redskins'),
+                                                ('ALL', 'All Teams')])
+    submit = SubmitField('Search')
+
+
+class SearchPlayerForm(FlaskForm):
+    fname = StringField('Player First Name', validators=[Optional()])
+    lname = StringField('Player Last Name', validators=[Optional()])
+    submit = SubmitField('Search')
+
+
+class PlayerReviewForm(FlaskForm):
+    text = TextAreaField('Comment', validators=[InputRequired(), Length(min=1, max=500)])
+    draftRound = IntegerField('Draft Round', validators=[InputRequired(), NumberRange(min=1, max=15,
+                                                                                      message='Draft Round must be between 1 and 15')])
+    playAgain = SelectField('Play Again', choices=[('YES', 'Yes'), ('NO', 'No')])
+    submit = SubmitField('Submit Review')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=40)])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.objects(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username is taken')
+
+    def validate_email(self, email):
+        user = User.objects(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email is taken')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=40)])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+    token = StringField('Token', validators=[InputRequired(), Length(min=6, max=6)])
+
+    submit = SubmitField('Login')
+
+    def validate_username(self, username):
+        user = User.objects(username=username.data).first()
+        if user is None:
+            raise ValidationError('Login failed. Check your username and/or password')
+
+    # Validate password?
+
+    def validate_token(self, token):
+        user = User.objects(username=self.username.data).first()
+        if user is not None:
+            tok_verified = pyotp.TOTP(user.otp_secret).verify(token.data)
+            if not tok_verified:
+                raise ValidationError('Invalid Token')
+            else:
+                print('success tho')
+        else:
+            raise ValidationError('Token User Error')
+
+
+class UpdateUsernameForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=40)])
+
+    submit = SubmitField('Update Username')
+
+    def validate_username(self, username):
+        user = User.objects(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username is taken')
---
+++ flask_app/models.py
@@ -0,0 +1,30 @@
+from flask_login import UserMixin
+from datetime import datetime
+from . import db, login_manager
+import pyotp
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(username=user_id).first()
+
+class User(db.Document, UserMixin):
+    email = db.EmailField(unique=True, required=True)
+    username = db.StringField(unique=True, required=True)
+    password = db.StringField()
+
+    # For 2FA
+    otp_secret = db.StringField(required=True, min_length=16, max_length=16, default=pyotp.random_base32())
+
+    # Returns unique string identifying our object
+    def get_id(self):
+        return self.username
+
+# Repurpose for fantasy football player reviews (not movies)
+class Review(db.Document):
+    commenter = db.ReferenceField(User)
+    content = db.StringField(min_length=1, max_length=500, required=True)
+    draftRound = db.IntField(required=True)
+    playAgain = db.StringField(required=True, min_length=2, max_length=3)
+    date = db.StringField(required = True)
+    player_name = db.StringField(required = True)
+    player_id = db.StringField(min_length=7, max_length=7, required=True)
