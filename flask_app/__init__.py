849ed1b3e9ca930b64d694e82d808dbda655fcd8
Siddhesh Gupta <siddhesh@terpmail.umd.edu>
2021-05-12 21:43:37 -0400

    Mongo DB commit

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -21,9 +21,7 @@ csp = {
 }
 Talisman(app, content_security_policy=csp)
 
-# app.config["MONGODB_HOST"] = 'mongodb://heroku_1g94z7ls:6jah3cqn8h46fqlogsjfhhic9m@ds161109.mlab.com:61109/heroku_1g94z7ls?retryWrites=false'
-app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/finalproject'
-# app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
+app.config['MONGODB_HOST'] = 'mongodb+srv://admin_user:5CxgEhwOL1pIMHfR@cluster0.2qwdu.mongodb.net/cmsc388j?retryWrites=true&w=majority'
 app.config['SECRET_KEY'] = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
 
 
